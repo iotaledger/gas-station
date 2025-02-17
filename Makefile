4ce604568f0909bca03e64a3624c694735fe427e
Pawel Iwan <pawel.ivan@iota.org>
2025-02-17 14:24:29 +0100

    chore: building process

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 redis-start:
-	docker run -d --name redis -p 6379:6379 redis:7.2.5
+	docker run -d --name gas-station-redis -p 6379:6379 redis:7.2.5
 
 redis-restart:
-	docker stop redis
-	docker rm redis -v
-	docker run -d --name redis -p 6379:6379 redis:7.2.5
+	docker stop gas-station-redis
+	docker rm gas-station-redis -v
+	docker run -d --name gas-station-redis -p 6379:6379 redis:7.2.5
+
+redis-stop:
+	docker stop gas-station-redis
+	docker rm gas-station-redis -v
 
--- docker/build.sh
+++ docker/build.sh
@@ -10,6 +10,8 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 DOCKERFILE="$DIR/Dockerfile"
 GIT_REVISION="$(git describe --always --dirty --exclude '*')"
 BUILD_DATE="$(date -u +'%Y-%m-%d')"
+ENTRY_BINARY="${ENTRY_BINARY:-iota-gas-station}"
+
 
 echo
 echo "Building iota-gas-station docker image"
@@ -17,9 +19,11 @@ echo "Dockerfile: \t$DOCKERFILE"
 echo "docker context: $REPO_ROOT"
 echo "build date: \t$BUILD_DATE"
 echo "git revision: \t$GIT_REVISION"
+echo "binary: \t$ENTRY_BINARY"
 echo
 
 docker build -f "$DOCKERFILE" "$REPO_ROOT" \
 	--build-arg GIT_REVISION="$GIT_REVISION" \
 	--build-arg BUILD_DATE="$BUILD_DATE" \
+	--build-arg ENTRY_BINARY="$ENTRY_BINARY" \
 	"$@"
