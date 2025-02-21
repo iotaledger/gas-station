3a2805aba99046a78d564187bae2c29755fb8483
Pawel Iwan <pawel.ivan@iota.org>
2025-02-21 10:02:09 +0100

    chore: add exec permissions

---
+++ utils/iota-gas-station.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+IMAGE_NAME="iotaledger/gas-station:latest"
+
+docker run --init -it --rm \
+      --name iota-gas-station \
+      --network host \
+      -v $(pwd):/app \
+      -w /app -u $(id -u):$(id -g) \
+      -e GAS_STATION_AUTH="$GAS_STATION_AUTH" \
+      ${IMAGE_NAME} "$@"
